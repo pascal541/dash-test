33e4fd899f0821d1e509106a6b6039b9d1149f54
solvedDev <solvedDev@gmail.com>
2022-09-14 15:06:06 +0200

    fix: error upon running dash

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
-import {Dash, initRuntimes} from 'dash-compiler'
-import {isMatch} from 'bridge-common-utils'
-import {NodeFileSystem} from './Dash/FileSystem'
-import {FileTypeImpl, PackTypeImpl} from './Dash/Definitions'
+import { Dash, initRuntimes } from 'dash-compiler'
+import { isMatch } from 'bridge-common-utils'
+import { NodeFileSystem } from './Dash/FileSystem'
+import { FileTypeImpl, PackTypeImpl } from './Dash/Definitions'
 import axios from 'axios'
 import path from 'path'
 
 build().then()
 
 async function createDashService() {
-    const input = new NodeFileSystem(path.join(__dirname, '..', 'test_project'))
-    const output = new NodeFileSystem(path.join(__dirname, '..', 'test_project'))
-    const dash = new Dash(input, output,
-        {
-            config: path.join(__dirname, '..', 'test_project', 'config.json'),
-            mode: 'production',
-            packType: <any>new PackTypeImpl(undefined),
-            fileType: <any>new FileTypeImpl(undefined, isMatch),
-            requestJsonData: (dataPath: string) =>
-                axios.get(
-                    dataPath.replace(
-                        'data/',
-                        'https://raw.githubusercontent.com/bridge-core/editor-packages/main/'
-                    )
-                ).then(resp => resp.data)
-        })
-    await dash.setup()
-    return dash
+	const input = new NodeFileSystem(path.join(__dirname, '..', 'test_project'))
+	const output = new NodeFileSystem(
+		path.join(__dirname, '..', 'test_project')
+	)
+	const dash = new Dash(input, output, {
+		config: path.join(__dirname, '..', 'test_project', 'config.json'),
+		mode: 'production',
+		packType: <any>new PackTypeImpl(undefined),
+		fileType: <any>new FileTypeImpl(undefined, isMatch),
+		requestJsonData: (dataPath: string) =>
+			axios
+				.get(
+					dataPath.replace(
+						'data/',
+						'https://raw.githubusercontent.com/bridge-core/editor-packages/main/'
+					)
+				)
+				.then((resp) => resp.data),
+	})
+	await dash.setup()
+	return dash
 }
 
 async function build() {
-    initRuntimes()
-    const dash = await createDashService()
-    await dash.build()
+	const dash = await createDashService()
+	await dash.build()
 }
